c15d2e85ab098b91663df51f570a23511528ce74
TIBOR <t.apr@seznam.cz>
2016-05-22 19:07:19 +0200

    Adventura-2016_05_15

---
+++ src/test/java/com/company/MapaTest.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import org.junit.Test;
+
+/**
+ * Created by TIBOR on 22.5.2016.
+ */
+public class MapaTest {
+    @Test
+    Mapa mapa;
+    Pozice aktualniPozice;
+
+    public void testMapa() throws Exception {
+
+        mapa.serialize(aktualniPozice);
+
+    }
+}
